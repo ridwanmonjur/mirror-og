2d83cae3cfec3907b12e61ef020b224f6d0447af
Ridwan Monjur <mjrrdn@gmail.com>
2023-09-10 23:54:57 +0600

    created user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name')->default();
+            $table->string('last_name')->default('');
             $table->string('name');
+            $table->string('display_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user');
+            $table->string('status')->default('inactive');
             $table->rememberToken();
             $table->timestamps();
         });
