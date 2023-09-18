e7993a0fd5afee831535428dffeab1f06b3eabe0
Ridwan Monjur <mjrrdn@gmail.com>
2023-09-18 22:13:16 +0600

    switch doesn't work this way

--- resources/views/Organizer/ManageEvent.blade.php
+++ resources/views/Organizer/ManageEvent.blade.php
@@ -114,20 +114,18 @@
                 $stylesEventStatus .= 'border: 1px solid ' . $mappingEventState[$event->eventStatus]['borderColor'] .' ; '; 
                 
                 $stylesEventRatio = '';
-                $ratio = $event->registeredParticipants / $event->totalParticipants;
-                switch ($ratio) {
-                    case ($ratio == 1):
-                        $stylesEventRatio .= "background-color: red; color: white;";
-                        break;
-                    case ($ratio == 0):
-                        $stylesEventRatio .= "background-color: #BFD684; color: white;";
-                        break;
-                    case ($ratio > 0.5):
-                        $stylesEventRatio .= "background-color: #FA831F; color: white;";
-                        break;
-                    case ($ratio <= 0.5):
-                        $stylesEventRatio .= "background-color: #FFE325; color: black;";
-                        break;
+                $ratio = (double) $event->registeredParticipants / $event->totalParticipants;
+                if ($ratio > 0.9){
+                    $stylesEventRatio .= "background-color: red; color: white;";
+                }
+                elseif ($ratio == 0){
+                    $stylesEventRatio .= "background-color: #8CCD39; color: white;";
+                }
+                elseif ($ratio > 0.5){
+                    $stylesEventRatio .= "background-color: #FA831F; color: white;";
+                }
+                elseif ($ratio <= 0.5){
+                    $stylesEventRatio .= "background-color: #FFE325; color: black;";
                 }
             @endphp
             <div 
