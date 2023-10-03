fed86326a4d1c9244091f42e152d32e9914ef982
Md Ariful Islam <arifulatwork@gmail.com>
2023-10-04 03:07:11 +0600

    filament update stats

--- app/Filament/Widgets/Dashboard/TotalEvent.php
+++ app/Filament/Widgets/Dashboard/TotalEvent.php
@@ -6,15 +6,17 @@ use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 use App\Models\EventDetail;
 use App\Models\EventCategory;
+use App\Models\User;
 
 class TotalEvent extends BaseWidget
 {
     protected function getCards(): array
     {
         return [
+            Card::make('Total User', User::count()),
             Card::make('Total Events', EventDetail::count()),
             Card::make('Total Event Category', EventCategory::count() ),
-            // Card::make('Average time on page', '3:12'),
+
         ];
     }
 }
