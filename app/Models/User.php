cba8722164220c0b8ca31ae5db54b20e4dcd1b6c
Ridwan Monjur <mjrrdn@gmail.com>
2023-10-09 19:52:54 +0600

    fix filament access

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
-class User extends Authenticatable
+use Filament\Panel;
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+   
+    public function canAccessFilament(): bool
+    {
+        return $this->role == 'ADMIN';
+    }
+    public static $filamentUserColumn = 'is_filament_user';
     /**
      * The attributes that are mass assignable.
      *
