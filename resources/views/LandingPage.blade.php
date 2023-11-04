eb02a9fd2ed2b490c613afef9ce99f9eee9e14e4
Ridwan Monjur <mjrrdn@gmail.com>
2023-11-04 15:33:25 +0600

    restore nafis landing page

--- resources/views/LandingPage.blade.php
+++ resources/views/LandingPage.blade.php
@@ -6,15 +6,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('assets/css/participant/player_home.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}"">
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> -->
     <link rel=" stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Splash</title>
     <script src="{{ asset('script.js') }}"></script>
 </head>
 
 <body>
+    <nav>
+        <div class="nav__sect1">
+            <img class="logo" src="{{ asset('/assets/images/logo2.png') }}" alt="">
+            <div class="search_box">
+                <i class="fa fa-search"></i>
+                <input class="nav__input" type="text" placeholder="Search for events">
+            </div>
+        </div>
 
-    <!-- @include('CommonLayout.Navbar') -->
+        <div class="hamburger-menu">
+            <i class="fa fa-bars"></i>
+        </div>
+        @guest
+        <div class="nav__items">
+            <ul>
+                <li><a href="#" id='nav-but' class="moop">Where's Moop?</a></li>
+                <div class="dropdown">
+                    <li><a onclick="myFunction()" style="color: white !important;" href="#" id='nav-but' class="dropbtn sign" data-bs-toggle="dropdown" aria-expanded="false">Sign In</a></li>
+                </div>
+                <div id="myDropdown" class="dropdown-content">
+                    <a class="dropdown-item" href="{{ route('organizer.signin.view') }}">Organizer</a>
+                    <a class="dropdown-item" href="{{ route('participant.signin.view') }}">Participant</a>
+                </div>
+
+            </ul>
+        </div>
+        @endguest
+        @auth
+        <div class="nav__items">
+            <ul>
+                <li><a href="#" id='nav-but' class="moop">Where's Moop?</a></li>
+                <li>
+                    <img style="position: relative; top: 0px; cursor: pointer;" width="50px" height="40px" src="{{ asset('/assets/images/navbar-account.png') }}" alt="">
+                </li>
+                <li>
+                    <img style="position: relative; top: 0px; left: -20px; cursor: pointer;" width="70px" height="40px" src="{{ asset('/assets/images/navbar-crown.png') }}" alt="">
+                </li>
+                <li>
+                    <a style="position: relative; top: 0px; left: -30px; cursor: pointer;" href="{{ route('logout.action') }}" class="moop">Sign Out</a>
+                </li>
+            </ul>
+        </div>
+        @endauth
+    </nav>
 
     <section class="hero">
         <img src="{{ asset('/assets/images/ss.png') }}" alt="">
@@ -71,5 +112,5 @@
         <p>Show More</p>
     </footer>
 </body>
-@stack('script')
+
 </html>
