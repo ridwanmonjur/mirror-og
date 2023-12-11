dbbd309479a3e4326acd0c41e22db987b2a5b0c9
Ridwan Monjur <mjrrdn@gmail.com>
2023-12-11 12:09:46 +0600

    restore last week code

--- resources/views/Organizer/Layout/CreateEventSuccess.blade.php
+++ resources/views/Organizer/Layout/CreateEventSuccess.blade.php
@@ -1,6 +1,7 @@
 @php
 
-$dateArray = bladeGenerateEventStartEndDateStr($event->startDate, $event->startTime);
+$status = $event->statusResolved();
+$dateArray = bladeGenerateEventStartEndDateStr($event->sub_action_public_date, $event->sub_action_public_time);
 
 extract($dateArray);
 
@@ -12,7 +13,13 @@ extract($dateArray);
         </u>
     </div>
     <div class="box-width">
+        @if ($event->$event->sub_action_public_time  && $event->sub_action_public_date)
         <p id="notification">Your <u>{{$event->sub_action_private}}</u> event has been scheduled to launch on {{$combinedStr}} at {{$timePart}}!</p>
+        @else @if($status!="DRAFT" && $status!= "PREVIEW")
+        <p id="notification">Your <u>{{$event->sub_action_private}}</u> event is already live!</p>
+        @else
+        <p id="notification">Your {{$status}} event has been created!</p>
+        @endif
     </div>
     <button class="js-shareUrl oceans-gaming-default-button" style="padding: 10px 50px; background-color: transparent; color: black; border: 1px solid black;">
         <img src="{{ asset('/assets/images/events/copy-icon.png') }}" class="js-shareUrl" height="20" width="20"> &nbsp;
@@ -20,7 +27,7 @@ extract($dateArray);
     </button>
     <br><br>
     <!-- <a style="" href="{{route('event.show', $event->id) }}"> <u> Click to more details for event type: {{$event->sub_action_private}} id: {{$event->id}} </u></a> -->
-    @if($event->sub_action_private == 'private')
+    @if($event->sub_action_private == 'private' )
     <a href="{{route('event.invitation.index', $event->id) }}">
         <button class="oceans-gaming-default-button" style="padding: 10px 50px; background-color: transparent; color: black; border: 1px solid black;">
             <img src="{{ asset('/assets/images/events/user.png') }}" height="20" width="20"> &nbsp;
