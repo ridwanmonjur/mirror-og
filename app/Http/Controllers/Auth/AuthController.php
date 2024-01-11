0566ce4f5ea85e7f67ccf9cfeb5103e1e26a780d
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-01-11 12:30:56 +0600

    modify search bar

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -379,7 +379,7 @@ class AuthController extends Controller
 
             Mail::send('Email.verify', ['token' => $token], function ($message) use ($user) {
                 $message->to($user->email);
-                $message->subject('Reset Password');
+                $message->subject('Email verification');
             });
 
             return redirect()
