284b83c48f1d38c416c1b3bc7ad0a7c06ad7d9d3
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-01-16 04:04:26 +0600

    ended

--- resources/views/Organizer/CreateEdit/CreateEventSuccess.blade.php
+++ resources/views/Organizer/CreateEdit/CreateEventSuccess.blade.php
@@ -1,8 +1,8 @@
 @php
 
-$status = $event->statusResolved();
-$dateArray = bladeGenerateEventStartEndDateStr($event->sub_action_public_date, $event->sub_action_public_time);
-extract($dateArray);
+    $status = $event->statusResolved();
+    $dateArray = bladeGenerateEventStartEndDateStr($event->sub_action_public_date, $event->sub_action_public_time);
+    extract($dateArray);
 
 @endphp
 <div class="text-center" id="step-13">
@@ -12,41 +12,59 @@ extract($dateArray);
         </u>
     </div>
     <div class="box-width">
-        @if ($event->sub_action_public_time && $event->sub_action_public_date)
-        <p id="notification">Your <u>{{$event->sub_action_private}}</u> event has been scheduled to launch on {{$combinedStr}} at {{$timePart}}!</p>
-        @elseif ($status!="DRAFT" && $status!= "PREVIEW" && $status!="ERROR")
-        <p id="notification">Your <u>{{$event->sub_action_private}}</u> event is already live!</p>
-        @else
-        <p id="notification">Your <u>{{strtolower($status)}}</u> event has been created!</p>
+        @if ($status == 'ERROR')
+            <p id="notification">Your <u>{{ strtolower($status) }}</u> event has no proper start date/ end date!</p>
         @endif
+        
+        @if ($status == 'DRAFT')
+            <p id="notification">Your <u>{{ strtolower($status) }}</u> event has been created!</p>
+        @endif
+
+        @if ($status == 'SCHEDULED')
+            <p id="notification">Your <u>{{ $event->sub_action_private }}</u> event has been scheduled to launch on
+                {{ $combinedStr }} at {{ $timePart }}!</p>
+        @endif
+
+        @if ($status == 'UPCOMING' || $status == 'ONGOING')
+            <p id="notification">Your <u>{{ $event->sub_action_private }}</u> event is alreadyP live!</p>
+        @endif
+
+        @if ($status == 'ENDED')
+            <p id="notification">Your <u>{{ $event->sub_action_private }}</u> event has ended</p>
+        @endif
+
     </div>
-    <button class="js-shareUrl oceans-gaming-default-button" style="padding: 10px 50px; background-color: transparent; color: black; border: 1px solid black;">
-        <img src="{{ asset('/assets/images/events/copy-icon.png') }}" class="js-shareUrl" height="20" width="20"> &nbsp;
+    <button class="js-shareUrl oceans-gaming-default-button"
+        style="padding: 10px 50px; background-color: transparent; color: black; border: 1px solid black;">
+        <img src="{{ asset('/assets/images/events/copy-icon.png') }}" class="js-shareUrl" height="20" width="20">
+        &nbsp;
         Copy event url
     </button>
     <br><br>
-    <!-- <a style="" href="{{route('event.show', $event->id) }}"> <u> Click to more details for event type: {{$event->sub_action_private}} id: {{$event->id}} </u></a> -->
-    @if($event->sub_action_private == 'private' )
-    <a href="{{route('event.invitation.index', $event->id) }}">
-        <button class="oceans-gaming-default-button" style="padding: 10px 50px; background-color: transparent; color: black; border: 1px solid black;">
-            <img src="{{ asset('/assets/images/events/user.png') }}" height="20" width="20"> &nbsp;
-            View Invite list
-        </button>
-    </a>
-    <br><br>
-    <br><br>
+    <!-- <a style="" href="{{ route('event.show', $event->id) }}"> <u> Click to more details for event type: {{ $event->sub_action_private }} id: {{ $event->id }} </u></a> -->
+    @if ($event->sub_action_private == 'private')
+        <a href="{{ route('event.invitation.index', $event->id) }}">
+            <button class="oceans-gaming-default-button"
+                style="padding: 10px 50px; background-color: transparent; color: black; border: 1px solid black;">
+                <img src="{{ asset('/assets/images/events/user.png') }}" height="20" width="20"> &nbsp;
+                View Invite list
+            </button>
+        </a>
+        <br><br>
+        <br><br>
     @endif
-    <button onclick="goToManageScreen();" class="oceans-gaming-default-button" style="padding: 10px 50px; background-color: white; color: black; border: 1px solid black;">
+    <button onclick="goToManageScreen();" class="oceans-gaming-default-button"
+        style="padding: 10px 50px; background-color: white; color: black; border: 1px solid black;">
         Go to event page
     </button>
     <br><br>
-    <a href="{{route('event.show', $event->id) }}">
+    <a href="{{ route('event.show', $event->id) }}">
         <button class="oceans-gaming-default-button" style="padding:10px 100px;">Done</button>
     </a>
 </div>
 <script>
     const goToManageScreen = () => {
-        window.location.href = "{{route('event.index') }}";
+        window.location.href = "{{ route('event.index') }}";
     }
     let copyUrl = "{{ route('event.show', $event->id) }}";
     const copyUtil = () => {
