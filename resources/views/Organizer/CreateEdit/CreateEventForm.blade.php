51844ea7b0d241090433c70cb90160337d744812
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-01-16 03:15:42 +0600

    bugger

--- resources/views/Organizer/CreateEdit/CreateEventForm.blade.php
+++ resources/views/Organizer/CreateEdit/CreateEventForm.blade.php
@@ -409,7 +409,7 @@
                         id="previewImage" alt="Preview" width="350px" height="auto"
                     >
                 @else
-                    <img class="banner-preview-img" id="previewImage" alt="Preview" width="350px"
+                    <img class="d-none sbanner-preview-img" id="previewImage" alt="Preview" width="350px"
                         height="auto">
                 @endif
             </div>
