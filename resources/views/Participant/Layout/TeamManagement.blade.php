7f77c50c596db77cab40f537e3d0ffae7bd944f0
Md Ariful Islam <arifulatwork@gmail.com>
2024-01-23 08:02:02 +0600

    Done Updated

--- resources/views/Participant/Layout/TeamManagement.blade.php
+++ resources/views/Participant/Layout/TeamManagement.blade.php
@@ -183,6 +183,36 @@
         </div>
         <div class="tab-content" id="CurrentMembers" data-type="member" style="display: none; text-align: center;">
         <p style="text-align: center;">Team {{ $manage->teamName }} has {{ $usernamesCount }} members</p>
+        <div class="cont">
+            <div class="leftC">
+                <span class="icon2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-filter">
+                        <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
+                    </svg>
+                    <span> Filter </span>
+                </span>
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <span class="icon2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M20.2 7.8l-7.7 7.7-4-4-5.7 5.7" />
+                        <path d="M15 7h6v6" />
+                    </svg>
+                    <span>
+                        Sort
+                    </span>
+                </span>
+            </div>
+
+            <div class="rightC">
+                <div class="search_box">
+                    <i class="fa fa-search"></i>
+                    <input class="nav__input" type="text" placeholder="Search for player name">
+                </div>
+                <div style="padding-right: 200px; transform: translateY(-95%);">
+                    <img src="/assets/images/add.png" height="40px" width="40px">
+                </div>
+            </div>
+        </div>
         <table class="member-table">
             <tbody>
                 {{-- Display the creator's name --}}
@@ -219,6 +249,36 @@
     </div>
     <div class="tab-content" id="PendingMembers" data-type="member" style="display: none; text-align: center;">
         <p style="text-align: center;">Pending Members</p>
+        <div class="cont">
+            <div class="leftC">
+                <span class="icon2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-filter">
+                        <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
+                    </svg>
+                    <span> Filter </span>
+                </span>
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <span class="icon2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M20.2 7.8l-7.7 7.7-4-4-5.7 5.7" />
+                        <path d="M15 7h6v6" />
+                    </svg>
+                    <span>
+                        Sort
+                    </span>
+                </span>
+            </div>
+
+            <div class="rightC">
+                <div class="search_box">
+                    <i class="fa fa-search"></i>
+                    <input class="nav__input" type="text" placeholder="Search for player name">
+                </div>
+                <div style="padding-right: 200px; transform: translateY(-95%);">
+                    <img src="/assets/images/add.png" height="40px" width="40px">
+                </div>
+            </div>
+        </div>
         <table class="member-table">
             <tbody>
                 @foreach($pendingMembers as $pendingMember)
