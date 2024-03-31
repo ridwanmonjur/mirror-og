0ba590ae1a26b41bd7ed8efb7d293b5fdf45466d
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-03-31 08:51:26 +0600

    push for re-strucutre

--- resources/views/Organizer/ManageEvent/ManageEventScroll.blade.php
+++ resources/views/Organizer/ManageEvent/ManageEventScroll.blade.php
@@ -129,18 +129,34 @@
                         &nbsp;
                         <span>{{ $event->type->eventType ?? 'Choose a type' }}</span>
                     </div>
-                    <div class="d-flex justify-content-center">
-                        @if (!in_array($status, ['PENDING', 'ENDED', 'DRAFT'])) 
-                            <a class="m-0 mt-2 mb-2 px-3 py-2 btn oceans-gaming-default-button text-light" href="{{ route('event.invitation.index', $event->id) }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar4-event mb-1" viewBox="0 0 16 16">
+                    <div class="d-flex justify-content-around">
+                        @if (!in_array($status, ['PENDING', 'DRAFT'])) 
+                            @if ($status != "ENDED")
+                                <a class="m-0 mt-2 mb-2 px-3 py-1 btn oceans-gaming-default-button text-light" href="{{ route('event.invitation.index', $event->id) }}">
+                                    {{-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar4-event mb-1" viewBox="0 0 16 16">
+                                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
+                                    <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+                                    </svg> --}}
+                                    <span> Invite </span>
+                                </a>
+                            @endif
+                            <a class="m-0 mt-2 mb-2 px-3 py-1 btn oceans-gaming-default-button text-light" href="{{ route('event.invitation.index', $event->id) }}">
+                                {{-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar4-event mb-1" viewBox="0 0 16 16">
                                 <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
                                 <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
-                                </svg>
-                                <span> Send invitation </span>
+                                </svg> --}}
+                                <span> Schedule </span>
                             </a>
-                        @else 
-                            <div class="d-flex justify-content-center align-items-center my-2">
-                                <span> Event is now {{strtolower($status)}}. </span>
+                            <a class="m-0 mt-2 mb-2 px-3 py-1 btn oceans-gaming-default-button text-light" href="{{ route('event.invitation.index', $event->id) }}">
+                                {{-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar4-event mb-1" viewBox="0 0 16 16">
+                                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
+                                <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+                                </svg> --}}
+                                <span> Results </span>
+                            </a>
+                        @else
+                            <div class="d-flex justify-content-center align-items-center my-2 py-2">
+                                <span> <i>Event is now {{strtolower($status)}}. </i> </span>
                             </div>
                         @endif
                     </div>
