6c7ce012602e7e4bda1ad056666775a7e8fc9e39
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-04-04 10:20:02 +0600

    purge css

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import purge from '@erbelion/vite-plugin-laravel-purgecss'
 
 export default defineConfig({
     plugins: [
@@ -10,5 +11,12 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        purge({
+            templates: ['blade'],
+            paths: ['resources/views/*']
+        })
     ],
+    build: { minify: true },
+    
+
 });
