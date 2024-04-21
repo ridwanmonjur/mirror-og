15bfb73dc6704b6046addec7324e6e28a72321c1
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-04-21 22:19:25 +0600

    custom css file rename for vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/sass/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
