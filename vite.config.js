9851e42e15b53c82fd32a164403da22efd30f08a
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-04-26 22:28:07 +0600

    remove alias

--- vite.config.js
+++ vite.config.js
@@ -16,9 +16,4 @@ export default defineConfig({
         })
     ],
     build: { minify: true },
-    resolve: {
-        alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
-    },
 });
