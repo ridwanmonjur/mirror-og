cd87178806a6688b8b151f74e0a5711ec2ec8b1f
ridwanmonjur <mjrrdn@gmail.com>
2024-05-10 05:47:25 +0000

    ammend unique key

--- database/migrations/2024_03_23_154306_change_invitation_table_with_team_concept.php
+++ database/migrations/2024_03_23_154306_change_invitation_table_with_team_concept.php
@@ -13,7 +13,6 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('event_invitations', function (Blueprint $table) {
-            $table->dropConstrainedForeignId('participant_id');
 
             $table->unsignedBigInteger('team_id')->nullable();
             $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
@@ -28,8 +27,6 @@ return new class extends Migration {
     public function down()
     {
         Schema::table('event_invitations', function (Blueprint $table) {
-            $table->unsignedBigInteger('participant_user_id')->nullable();
-            $table->foreign('participant_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->dropConstrainedForeignId('team_id');
         });
