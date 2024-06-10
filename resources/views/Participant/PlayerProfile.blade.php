6fc601b70ab848a102c2b2c3188b49b04fc77d6e
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-06-10 15:14:58 +0600

    deal with friends

--- resources/views/Participant/PlayerProfile.blade.php
+++ resources/views/Participant/PlayerProfile.blade.php
@@ -56,7 +56,6 @@
             ])
         >
             @if(!$isOwnProfile) 
-                <br> 
             @else
                 <input type="hidden" id="userBannerInput" value="{{ $userProfile->userBanner }}">
                 <input type="hidden" id="backgroundColorInput" value="{{ $userProfile->backgroundColor }}">
@@ -66,7 +65,6 @@
             @if($isOwnProfile)
                 <div class="d-flex justify-content-end py-0 my-0 mb-2">
                     <input type="file" id="backgroundInput" class="d-none"> 
-                    @include('Participant.Profile.FriendManagement')
                     <button 
                         x-show="!isEditMode"
                         data-bs-toggle="modal"
@@ -102,6 +100,10 @@
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                     </svg>
+                </div>
+            @else
+                <div class="d-flex justify-content-end py-0 my-0 mb-2" style="font-size: 12px;">
+                    @include('Participant.Profile.FriendManagement')
                 </div>
              @endif
             <div class="d-flex justify-content-center align-items-start flex-wrap">
