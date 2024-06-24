5c643fbef43441ff2ce64cdb774b7721bcc13e04
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-06-25 05:29:13 +0600

    underline animation

--- vite.config.js
+++ vite.config.js
@@ -20,5 +20,16 @@ export default defineConfig({
         }),
        
     ],
-    build: { minify: true },
+    build: { 
+        minify: true, 
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    },
 });
