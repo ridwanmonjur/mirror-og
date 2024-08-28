96306ddcf71f39cbaecab0d8b9a4c6ae50601749
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-08-28 08:37:08 +0600

    experiment swup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,18 @@
- import Swal from 'sweetalert2'
+// import Swup from 'swup';
+// import SwupHeadPlugin from '@swup/head-plugin';
+// import SwupA11yPlugin from '@swup/a11y-plugin';
+
+// new Swup({
+//     plugins: [new SwupHeadPlugin(), new SwupA11yPlugin()]
+// });
+
+
+import Swal from 'sweetalert2'
  import * as Popper from '@popperjs/core'
  window.Popper = Popper
  import * as bootstrap from 'bootstrap'
- window.bootstrap = bootstrap
-
-
+ window.bootstrap = bootstrap;
+ 
 window.loadBearerHeader = function() {
     return {
         credentials: 'include'
@@ -97,8 +105,6 @@ window.loadMessage = () => {
     localStorage.removeItem('tab');
 }
 
-import Swup from 'swup';
-const swup = new Swup();
 
 window.Toast = Toast;
 window.Swal = Swal;
