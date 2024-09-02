ea828700dec729ce829c8f3ae5b0128dd6192103
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-09-02 11:04:12 +0600

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tournamnet Demo</title>
+    <title>Tournament Demo</title>
     <link rel="stylesheet" href="{{ asset('/assets/css/common/tournament.css') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @include('__CommonPartials.HeadIcon')
