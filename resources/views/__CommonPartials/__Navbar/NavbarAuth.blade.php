db8b476fdccb2de1332a2b576f5348ce31564b32
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-09-06 17:04:42 +0600

    After clicking done, user should be redirected to event details page for the event they just joined

--- resources/views/__CommonPartials/__Navbar/NavbarAuth.blade.php
+++ resources/views/__CommonPartials/__Navbar/NavbarAuth.blade.php
@@ -36,11 +36,12 @@
                 </div>
                 @if ($nextCursor)
                     <div class="d-flex pagination justify-content-center mx-auto">
-                        <button class="mb-4 btn btn-link text-light btn-sm" id="load-more" 
+                        <button class="btn btn-link text-light btn-sm" id="load-more" 
                             data-url="{{ route('user.notifications.more', ['id' => $user->id]) }}" 
                             data-cursor="{{ $nextCursor ? $nextCursor->encode() : '' }}"
                         > <u>Load More </u></button>
                     </div>
+                    <br>
                 @endif
             </div> 
         </div>
