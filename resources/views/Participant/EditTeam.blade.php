cb88f43d56e4b465f16c198aeabfa3c4029334de
Ridwan Bin Monjur <mjrrdn@gmail.com>
2024-12-08 15:16:38 +0600

    placeholder bug

--- resources/views/Participant/EditTeam.blade.php
+++ resources/views/Participant/EditTeam.blade.php
@@ -51,11 +51,9 @@
                             @endif
 
                             <div class="d-flex flex-column align-items-center justify-content-center">
-                                <input type="text" value="{{ $team ? $team->teamName : '' }}" name="teamName" id="teamName" placeholder="Team Name"
-                                    onclick="clearPlaceholder(this)" onblur="restorePlaceholder(this)">
+                                <input type="text" value="{{ $team ? $team->teamName : '' }}" name="teamName" id="teamName" placeholder="Team name">
                                 <input type="text" style="height: 100px;" value="{{ $team ? $team->teamDescription : '' }}" name="teamDescription"
-                                    id="teamDescription" placeholder="Write your team description..." onclick="clearPlaceholder(this)"
-                                    onblur="restorePlaceholder(this)">
+                                    id="teamDescription" placeholder="Write your team description...">
                                 <br> <br>
                                 <input type="submit" onclick="" value="Edit">
                                 
