9a5f7a32d3f8f65ab9352ec935b5fd7571ccc063
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-01-19 20:11:58 +0600

    feat(bug): fix

--- public/assets/js/participant/TeamManagementMemberView.js
+++ public/assets/js/participant/TeamManagementMemberView.js
@@ -320,7 +320,7 @@ async function fetchMembers(event = null) {
                             d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
                     </svg>
                 </td>
-                <td class="coloured-cell px-2">
+                <td class="coloured-cell px-2" onclick="redirectToProfilePage('${member.user_id}');">
                     <div class="player-info">
                             <div class="${!captainJson || member.id != captainJson?.team_member_id && 'd-none'} player-image">
                             </div>
