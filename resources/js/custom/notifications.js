92f60db13b5cef30edadbbd009d289112308187e
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-02-26 15:07:39 +0600

    red

--- resources/js/custom/notifications.js
+++ resources/js/custom/notifications.js
@@ -200,7 +200,7 @@ const iconStore = {
     
     live: `<svg width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#dc3545" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M23 7l-7 5 7 5V7z"></path><rect x="3" y="5" width="14" height="14" rx="2" ry="2"></rect></svg>`,
 
-    ended: `<svg width="30" height="30"  viewBox="0 0 24 24" fill="none" stroke="#6c757d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    ended: `<svg width="30" height="30"  viewBox="0 0 24 24" fill="none" stroke="orange" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
     <!-- Clock face -->
     <circle cx="12" cy="12" r="10"></circle>
     
