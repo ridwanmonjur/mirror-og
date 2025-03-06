81de964033daac0e0d1a18e9d5d7b8bc13735a77
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-03-06 21:28:11 +0600

    evr

--- resources/views/Organizer/EventResults.blade.php
+++ resources/views/Organizer/EventResults.blade.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="{{ asset('/assets/css/participant/teamAdmin.css') }}">
     <link rel="stylesheet" href="{{ asset('/assets/css/organizer/event-creation.css') }}">
     @include('__CommonPartials.HeadIcon')
-    <link rel="stylesheet" href="{{ asset('/assets/css/organizer/eventResults.css') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
 </head>
