a2ed05f93e038f17a5844ac1a051e250b51f65f1
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-03-26 14:09:26 +0600

    unnecessary check

--- app/Http/Controllers/Shared/FirebaseController.php
+++ app/Http/Controllers/Shared/FirebaseController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Services\Firebase;
+use Exception;
 use Illuminate\Support\Facades\Log;
 use Kreait\Firebase\Contract\Auth;
 use Kreait\Firebase\Contract\Firestore;
@@ -67,85 +68,91 @@ class FirebaseController extends Controller
 
     public function toggleBlock(Request $request, $id): JsonResponse
     {
-        $authenticatedUser = $request->attributes->get('user');
-        $user = User::where('id', $id)
-            ->select('id')
-            ->first();
-        
-        if ($authenticatedUser->id == $user->id) {
-
-            return response()->json([
-                'message' => "Can't block yourself",
-                'is_blocked' => "False"
-            ], 404);
-        }
-        
-        if ($authenticatedUser->hasBlocked($user)) {
-            $authenticatedUser->blocks()->detach($user);
-            $message = 'User unblocked successfully';
-            $isBlocked = false;
-
-            $roomCollectionRef = $this->firestore->database()->collection('room');
-
-            $query1 = $roomCollectionRef->where('user1', '==', (string)$user->id)
-                ->where('user2', '==', (string)$authenticatedUser->id)
-                ->where('blocked_by', '==', (string)$authenticatedUser->id);
-
-            $query2 = $roomCollectionRef->where('user2', '==', (string)$user->id)
-                ->where('user1', '==', (string)$authenticatedUser->id)
-                ->where('blocked_by', '==', (string)$authenticatedUser->id);
-
-            $snapshot1 = $query1->documents();
-            $snapshot2 = $query2->documents();
-           
-            foreach ($snapshot1 as $document) {
-                $roomRef = $roomCollectionRef->document($document->id());
-                $roomRef->update([
-                    ['path' => 'blocked_by', 'value' => null]
-                ]);            
+        try {
+            $authenticatedUser = $request->attributes->get('user');
+            $user = User::where('id', $id)
+                ->select('id')
+                ->first();
+            
+            if ($authenticatedUser->id == $user->id) {
+                return response()->json([
+                    'message' => "Can't block yourself",
+                    'is_blocked' => "False"
+                ], 404);
             }
+            if ($authenticatedUser->hasBlocked($user)) {
+                $authenticatedUser->blocks()->detach($user);
+                $message = 'User unblocked successfully';
+                $authenticatedUser->save();
+                $isBlocked = false;
 
-            foreach ($snapshot2 as $document) {
-                $roomRef = $roomCollectionRef->document($document->id());
-                $roomRef->update([
-                    ['path' => 'blocked_by', 'value' => null]
-                ]);
-            }
-        } else {
-            $authenticatedUser->blocks()->attach($user);
-            $message = 'User blocked successfully';
-            $isBlocked = true;
-
-            if (!$user->hasBlocked($authenticatedUser)) { 
                 $roomCollectionRef = $this->firestore->database()->collection('room');
-                
+
                 $query1 = $roomCollectionRef->where('user1', '==', (string)$user->id)
                     ->where('user2', '==', (string)$authenticatedUser->id);
-                
+
                 $query2 = $roomCollectionRef->where('user2', '==', (string)$user->id)
                     ->where('user1', '==', (string)$authenticatedUser->id);
-                
+
                 $snapshot1 = $query1->documents();
                 $snapshot2 = $query2->documents();
-
+            
                 foreach ($snapshot1 as $document) {
                     $roomRef = $roomCollectionRef->document($document->id());
                     $roomRef->update([
-                        ['path' => 'blocked_by', 'value' => $authenticatedUser->id]
-                    ]);                }
-                
+                        ['path' => 'blocked_by', 'value' => null]
+                    ]);            
+                }
+
                 foreach ($snapshot2 as $document) {
                     $roomRef = $roomCollectionRef->document($document->id());
                     $roomRef->update([
-                        ['path' => 'blocked_by', 'value' => $authenticatedUser->id]
-                    ]);                
+                        ['path' => 'blocked_by', 'value' => null]
+                    ]);
+                }
+            } else {
+                
+                $authenticatedUser->blocks()->attach($user);
+                $authenticatedUser->save();
+                $message = 'User blocked successfully';
+                $isBlocked = true;
+
+                if (!$user->hasBlocked($authenticatedUser)) { 
+                    $roomCollectionRef = $this->firestore->database()->collection('room');
+                    
+                    $query1 = $roomCollectionRef->where('user1', '==', (string)$user->id)
+                        ->where('user2', '==', (string)$authenticatedUser->id);
+                    
+                    $query2 = $roomCollectionRef->where('user2', '==', (string)$user->id)
+                        ->where('user1', '==', (string)$authenticatedUser->id);
+                    
+                    $snapshot1 = $query1->documents();
+                    $snapshot2 = $query2->documents();
+
+                    foreach ($snapshot1 as $document) {
+                        $roomRef = $roomCollectionRef->document($document->id());
+                        $roomRef->update([
+                            ['path' => 'blocked_by', 'value' => $authenticatedUser->id]
+                        ]);                }
+                    
+                    foreach ($snapshot2 as $document) {
+                        $roomRef = $roomCollectionRef->document($document->id());
+                        $roomRef->update([
+                            ['path' => 'blocked_by', 'value' => $authenticatedUser->id]
+                        ]);                
+                    }
                 }
             }
-        }
 
-        return response()->json([
-            'message' => $message,
-            'is_blocked' => $isBlocked
-        ]);
+            return response()->json([
+                'message' => $message,
+                'is_blocked' => $isBlocked
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+                'success' => false
+            ]);
+        }
     }
 }
