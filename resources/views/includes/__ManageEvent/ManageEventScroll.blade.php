e6309917b1ca27181e249cc8c002f3d94a4ac673
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-04-03 19:08:43 +0600

    bug(D-000018-EVM-00): mb-2

--- resources/views/includes/__ManageEvent/ManageEventScroll.blade.php
+++ resources/views/includes/__ManageEvent/ManageEventScroll.blade.php
@@ -198,7 +198,7 @@
             <div class="group-hover flex-wrap d-flex justify-content-center cursor-pointer mb-2">
                    
                     @if (in_array($status, ['UPCOMING', 'DRAFT', 'SCHEDULED']))
-                        <a class="m-0 mb-1 mx-4 p-0" href="{{ route('event.show', $event->id) }}">
+                        <a class="m-0 mb-2 mx-4 p-0" href="{{ route('event.show', $event->id) }}">
                             <svg onclick="goToLivePreview()" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
                             <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
                             <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
@@ -206,26 +206,26 @@
                         </a>
                         <a 
                             data-event-id="{{$event->id}}"
-                            class="share-button m-0 mb-1 mx-4 p-0" >
+                            class="share-button m-0 mb-2 mx-4 p-0" >
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
                             <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/>
                             </svg>
                         </a>
                         @if ($status !== 'ONGOING')
-                            <a class="m-0 mb-1 mx-4 p-0"  href="{{ route('event.edit', $event->id) }}">
+                            <a class="m-0 mb-2 mx-4 p-0"  href="{{ route('event.edit', $event->id) }}">
                                 <svg onclick="goToEditScreen()" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
                                 </svg>
                             </a>
                         @endif
                     @elseif ($status === "PENDING")
-                        <a class="m-0 mx-4 mb-1 p-0"  href="{{ route('organizer.checkout.view', $event->id) }}">
+                        <a class="m-0 mx-4 mb-2 p-0"  href="{{ route('organizer.checkout.view', $event->id) }}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-check" viewBox="0 0 16 16">
                             <path d="M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"/>
                             <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
                             </svg>
                         </a>
-                        <a class="m-0 mx-4 mb-1 p-0"  href="{{ route('event.edit', $event->id) }}">
+                        <a class="m-0 mx-4 mb-2 p-0"  href="{{ route('event.edit', $event->id) }}">
                             <svg onclick="goToEditScreen()" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                             <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
                             </svg>
