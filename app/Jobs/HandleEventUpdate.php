87d2f41954e827fbee0ef99f09da1d9923bc9e8f
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-04-08 16:44:45 +0600

    bug(evr): change reg

--- app/Jobs/HandleEventUpdate.php
+++ app/Jobs/HandleEventUpdate.php
@@ -32,44 +32,47 @@ class HandleEventUpdate implements ShouldQueue
     public function handle()
     {
         try {
+            Log::info($this->eventDetail);
             $joinEvent = JoinEvent::where('event_details_id', $this->eventDetail->id)
                 ->where('join_status', 'confirmed')
                 ->with(['roster', 'roster.user'])
                 ->first();
+            Log::info($joinEvent);
             
             $memberMail = [];
             $this->eventDetail->load(['user']);
+            $startTimeDate = $this->eventDetail->createCarbonDateTime($this->eventDetail->startDate, $this->eventDetail->startTime);
+            $endTimeDate = $this->eventDetail->createCarbonDateTime($this->eventDetail->endDate, $this->eventDetail->startTime);
+
+            $partialHtml = <<<HTML
+                The event, <button class="btn-transparent px-0 border-0 Color-{$this->eventDetail->tier->eventTier}" data-href="/event/{$this->eventDetail->id}">{$this->eventDetail->eventName}</button> has
+                been RESCHEDULED. It starts on {$startTimeDate->format('l, F j, Y')} MYT and ends on {$endTimeDate->format('l, F j, Y')} MYT.
+                HTML;
+
+            $partialEmail = <<<HTML
+                The date and time for your event, <span class="px-0 border-0 notification-blue">{$this->eventDetail->eventName}</span> has been updated to the following:<br><br>
+                <b>Start: {$startTimeDate->format('l, jS F Y')}</b><br> <b>End: {$endTimeDate->format('l, jS F Y')}</b><br><br>
+                All registered teams have been notified of this change.
+                HTML;
+
+            $memberHtml = <<<HTML
+                <span class="notification-gray">
+                    {$partialHtml}
+                </span>
+                HTML;
+
+            $memberEmail = <<<HTML
+                <span class="notification-gray">
+                    {$partialEmail}
+                </span>
+                HTML;
+
 
             if ($joinEvent) {
-                $startTimeDate = $this->eventDetail->createCarbonDateTime($this->eventDetail->startDate, $this->eventDetail->startTime);
-                $endTimeDate = $this->eventDetail->createCarbonDateTime($this->eventDetail->endDate, $this->eventDetail->startTime);
-
+                
                 DB::beginTransaction();
                 try {
-
-                    $partialHtml = <<<HTML
-                        The event, <button class="btn-transparent px-0 border-0 Color-{$this->eventDetail->tier->eventTier}" data-href="/event/{$this->eventDetail->id}">{$this->eventDetail->eventName}</button> has
-                        been RESCHEDULED. It starts on {$startTimeDate->format('l, F j, Y')} MYT and ends on {$endTimeDate->format('l, F j, Y')} MYT.
-                        HTML;
-
-                    $partialEmail = <<<HTML
-                        The date and time for your event, <span class="px-0 border-0 notification-blue">{$this->eventDetail->eventName}</span> has been updated to the following:<br>
-                        <b>Start: {$startTimeDate->format('l, jS F Y')} End: {$endTimeDate->format('l, jS F Y')}</b><br>
-                        All registered teams have been notified of this change.
-                        HTML;
-
-                    $memberHtml = <<<HTML
-                        <span class="notification-gray">
-                            {$partialHtml}
-                        </span>
-                        HTML;
-
-                    $memberEmail = <<<HTML
-                        <span class="notification-gray">
-                            {$partialEmail}
-                        </span>
-                        HTML;
-
+                  
                     $notificationMap = [
                         'member' => [
                             'type' => 'event',
@@ -89,18 +92,13 @@ class HandleEventUpdate implements ShouldQueue
                         ]
                     ];
 
-                    $organizerEmail = new EventRescheduledMail([
-                        'text' => $notificationMap['organizer']['mail'],
-                        'link' =>  $notificationMap['organizer']['link'],
-                    ]);
-
                     $participantEmail = new EventRescheduledMail([
                         'text' => $notificationMap['member']['mail'],
                         'link' =>  $notificationMap['member']['link'],
                     ]);
 
 
-                    $memberNotification = $organizerNotification = [];
+                    $memberNotification =  [];
                     foreach ($joinEvent->roster as $member) {
                         $memberNotification[] = [
                             'user_id' => $member->user->id,
@@ -118,27 +116,47 @@ class HandleEventUpdate implements ShouldQueue
 
                     Mail::to($memberMail)->send($participantEmail);
 
-                    $organizerNotification = [
-                        'user_id' => $joinEvent->eventDetails->user_id,
-                        'type' => $notificationMap['organizer']['type'],
-                        'link' =>  $notificationMap['organizer']['link'],
-                        'icon_type' => $notificationMap['organizer']['icon_type'],
-                        'html' => $notificationMap['organizer']['html'],
-                        'created_at' => DB::raw('NOW()')
-                    ];
                     
-                    if ($this->eventDetail->user->email) Mail::to($this->eventDetail->user->email)->send($organizerEmail);
-
-                    NotifcationsUser::insertWithCount([
-                        ...$memberNotification,
-                        $organizerNotification
-                    ]);
+                    NotifcationsUser::insertWithCount($memberNotification);
 
                     DB::commit();
                 } catch (Exception $e) {
                     DB::rollBack();
                     Log::error($e->getMessage() . $e->getTraceAsString());
                 }
+            } else {
+                $notificationMap = [
+                    'organizer' => [
+                        'type' => 'event',
+                        'link' =>  route('public.event.view', ['id' => $this->eventDetail->id]),
+                        'icon_type' => 'schedule',
+                        'html' => $memberHtml,
+                        'mail' => $memberEmail,
+                        'created_at' => DB::raw('NOW()')
+                    ]
+                ];
+
+                $organizerEmail = new EventRescheduledMail([
+                    'text' => $notificationMap['organizer']['mail'],
+                    'link' =>  $notificationMap['organizer']['link'],
+                ]);
+
+                if ($this->eventDetail->user->email) Mail::to($this->eventDetail->user->email)->send($organizerEmail);
+                
+                $organizerNotification = [
+                    'user_id' => $joinEvent->eventDetails->user_id,
+                    'type' => $notificationMap['organizer']['type'],
+                    'link' =>  $notificationMap['organizer']['link'],
+                    'icon_type' => $notificationMap['organizer']['icon_type'],
+                    'html' => $notificationMap['organizer']['html'],
+                    'created_at' => DB::raw('NOW()')
+                ];
+                
+
+                NotifcationsUser::insertWithCount([
+                    $organizerNotification
+                ]);
+                
             }
         } catch (Exception $e) {
             Log::error($e->getMessage() . $e->getTraceAsString());
