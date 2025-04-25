1347b3234a3880f41038f53c4018bbecad53f986
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-04-25 20:06:44 +0600

    span

--- resources/views/includes/__BracketModal/__Report/ExistingChoices.blade.php
+++ resources/views/includes/__BracketModal/__Report/ExistingChoices.blade.php
@@ -1,56 +1,56 @@
 <template v-if="report.teams[reportUI.otherTeamNumber]?.winners[reportUI.matchNumber] || report.teams[reportUI.teamNumber]?.winners[reportUI.matchNumber]">
-    <small class="d-block">
+    <span class="d-block">
 
         <template v-if="report.teams[0]?.winners[reportUI.matchNumber]">
-            <small class="d-block">
-                <small v-text="report.teams[0].name"> </small>
+            <span class="d-block">
+                <span v-text="report.teams[0].name"> </span>
                 declared
-                <small class="text-primary" v-text="report.teams[report.teams[0].winners[reportUI.matchNumber]].name"> </small>
-                to be the winner for Game <small v-text="reportUI.matchNumber+1"> </small>
-            </small>
+                <span class="text-primary" v-text="report.teams[report.teams[0].winners[reportUI.matchNumber]].name"> </span>
+                to be the winner for Game <span v-text="reportUI.matchNumber+1"> </span>
+            </span>
         </template>
 
         <template v-if="report.teams[1].winners[reportUI.matchNumber] ">
-            <small class="d-block">
-                <small v-text="report.teams[1]?.name"> </small>       
+            <span class="d-block">
+                <span v-text="report.teams[1]?.name"> </span>       
                 declared
-                <small  class="text-primary" v-text="report.teams[report.teams[1]?.winners[reportUI.matchNumber]]?.name"> </small>
-                to be the winner for Game <small v-text="reportUI.matchNumber+1"> </small>
-            </small>
+                <span  class="text-primary" v-text="report.teams[report.teams[1]?.winners[reportUI.matchNumber]]?.name"> </span>
+                to be the winner for Game <span v-text="reportUI.matchNumber+1"> </span>
+            </span>
         </template>
 
         <template v-if="report.organizerWinners[reportUI.matchNumber]">
             <div>
-                <small>
+                <span>
                     Organizer has chosen
-                    <small  class="text-primary" v-text="report.teams[report.organizerWinners[reportUI.matchNumber]].name"> </small>
-                    to be the winner for Game <small v-text="reportUI.matchNumber+1"> </small>
-                </small>
+                    <span  class="text-primary" v-text="report.teams[report.organizerWinners[reportUI.matchNumber]].name"> </span>
+                    to be the winner for Game <span v-text="reportUI.matchNumber+1"> </span>
+                </span>
             </div>
         </template>
-    </small>
+    </span>
 </template>
 <template v-else>
-    <small class="d-block">
+    <span class="d-block">
         <template v-if="report.deadline.has_ended">
-            <small class="d-block">
+            <span class="d-block">
                 <span>No winner declared by either team. 
                     <template v-if="report.organizerWinners && report.organizerWinners[reportUI.matchNumber]">
                         <div>
-                            <small>
+                            <span>
                                 Winner declared by organizer.
-                            </small>
+                            </span>
                         </div>
                     </template>
                     <template v-if="report.randomWinners && report.randomWinners[reportUI.matchNumber]">
                         <div>
-                            <small>
+                            <span>
                                 Winner automatically chosen at random.
-                            </small>
+                            </span>
                         </div>
                     </template>
                 </span>
-            </small>
+            </span>
         </template>
-    </small>
+    </span>
 </template>
