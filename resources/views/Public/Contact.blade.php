0783b0fde069f8594f9b48bd85d1152cbe292dfd
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-05-12 14:09:44 +0600

    Revert "layouts"
    This reverts commit 2498767b6bd939dca6124015381d9ba71a4bcc4f.

--- resources/views/Public/Contact.blade.php
+++ resources/views/Public/Contact.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layout.app')
 
 @section('title', 'Driftwood - Contact Us')
 @section('body-class', 'contact')
--- resources/views/Public/Verify.blade.php
+++ resources/views/Public/Verify.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layout.app')
 
 @section('title', 'Driftwood - About Us')
 @section('body-class', 'about')
