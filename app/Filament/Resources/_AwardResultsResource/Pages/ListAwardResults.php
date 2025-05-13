72e5da95c78a70247e24a41ffd6831fbe9b556b0
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-05-13 12:04:50 +0600

    remove

---
+++ app/Filament/Resources/_AwardResultsResource/Pages/ListAwardResults.php
@@ -0,0 +1,19 @@
+<?php
+
+// namespace App\Filament\Resources\AwardResultsResource\Pages;
+
+// use App\Filament\Resources\AwardResultsResource;
+// use Filament\Actions;
+// use Filament\Resources\Pages\ListRecords;
+
+// class ListAwardResults extends ListRecords
+// {
+//     protected static string $resource = AwardResultsResource::class;
+
+//     protected function getHeaderActions(): array
+//     {
+//         return [
+//             Actions\CreateAction::make(),
+//         ];
+//     }
+// }
