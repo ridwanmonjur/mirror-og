888dbc1ae293b6a10c83424271ad8c6ab86032d8
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-05-26 08:55:04 +0600

    book keeping

--- app/Filament/Resources/_StarsResource/Pages/EditStars.php
+++ app/Filament/Resources/_StarsResource/Pages/EditStars.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace App\Filament\Resources\StarsResource\Pages;
+// namespace App\Filament\Resources\StarsResource\Pages;
 
-use App\Filament\Resources\StarsResource;
-use Filament\Actions;
-use Filament\Resources\Pages\EditRecord;
+// use App\Filament\Resources\StarsResource;
+// use Filament\Actions;
+// use Filament\Resources\Pages\EditRecord;
 
-class EditStars extends EditRecord
-{
-    protected static string $resource = StarsResource::class;
+// class EditStars extends EditRecord
+// {
+//     protected static string $resource = StarsResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\DeleteAction::make(),
-        ];
-    }
-}
+//     protected function getHeaderActions(): array
+//     {
+//         return [
+//             Actions\DeleteAction::make(),
+//         ];
+//     }
+// }
--- app/Filament/Resources/_StarsResource/Pages/ListStars.php
+++ app/Filament/Resources/_StarsResource/Pages/ListStars.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace App\Filament\Resources\StarsResource\Pages;
+// namespace App\Filament\Resources\StarsResource\Pages;
 
-use App\Filament\Resources\StarsResource;
-use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
+// use App\Filament\Resources\StarsResource;
+// use Filament\Actions;
+// use Filament\Resources\Pages\ListRecords;
 
-class ListStars extends ListRecords
-{
-    protected static string $resource = StarsResource::class;
+// class ListStars extends ListRecords
+// {
+//     protected static string $resource = StarsResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
-}
+//     protected function getHeaderActions(): array
+//     {
+//         return [
+//             Actions\CreateAction::make(),
+//         ];
+//     }
+// }
