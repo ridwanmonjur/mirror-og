4546fda248806a37a0d6fdb8d069539bc0e3d721
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-06-19 17:31:52 +0600

    push for testing

--- app/Models/EventDetail.php
+++ app/Models/EventDetail.php
@@ -702,7 +702,7 @@ class EventDetail extends Model implements Feedable
         ->withCount(
         ['joinEvents' => function ($q) {
             $q->where('join_status', 'confirmed');
-        }]);
+        ->orderBy('startDate', 'asc');
     }
 
     public static function filterEventsFull(Request $request)
