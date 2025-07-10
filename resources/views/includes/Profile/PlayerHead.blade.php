b8374147f46354135f657ffa1fed4bc3e0ab3c6a
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-07-10 23:57:34 +0600

    revert bg photo

--- resources/views/includes/Profile/PlayerHead.blade.php
+++ resources/views/includes/Profile/PlayerHead.blade.php
@@ -53,7 +53,7 @@
                             <a aria-hidden="true" data-fslightbox="lightbox"
                                 data-href="{{ '/' . 'storage/' . $userProfile->userBanner }}"
                                 class="hyperlink-lightbox">
-                                <button class="btn simple-button btn-sm p-0 me-2"><svg xmlns="http://www.w3.org/2000/svg"
+                                <button class="btn simple-button btn-sm p-0 "><svg xmlns="http://www.w3.org/2000/svg"
                                         width="20" height="20" fill="currentColor" class="bi bi-eye"
                                         viewBox="0 0 16 16">
                                         <path
@@ -64,7 +64,7 @@
                                 </button>
                             </a>
                             @if ($isUserSame)
-                                <button v-cloak v-show="isEditMode" id="upload-button2" class="btn btn-sm simple-button p-0"
+                                <button v-cloak v-show="isEditMode" id="upload-button2" class="btn btn-sm simple-button mx-2 p-0"
                                     aria-hidden="true">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                         fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
@@ -72,6 +72,13 @@
                                             d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                         <path fill-rule="evenodd"
                                             d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                    </svg>
+                                </button>
+                                <button v-cloak v-if="isEditMode" v-on:click="removeProfile(event);" id="trash-button3" class="btn btn-sm simple-button  p-0"
+                                    aria-hidden="true">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                                     </svg>
                                 </button>
                             @endif
@@ -333,3 +340,4 @@
         </div>
     </div>
 </div>
+
