90e981beb65ecc14fc06b3c94eddae7f11097be7
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-07-28 13:15:31 +0600

    pp

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -140,80 +140,89 @@
 
                     @forelse ($products as $product)
                         <div class="col-12 col-md-6 col-lg-4 col-xxl-3 mb-4">
-                        <a href="{{ route('shop.show', $product->slug) }}">
-                            
-                            <div style="border-radius: 30px; background-color: rgba(255, 255, 255, 0.7);" class="product-card border border-3 border-primary pb-4 h-100 d-flex flex-column">
-                                <div class="product-card__image-wrapper  mb-3">
-                                    
-                                    <img src="{{ asset('storage/' . $product->image) }}" 
-                                        class="product-card__image w-100 object-fit-cover   border-primary border-bottom"
-                                        style="height: 200px;border-radius: 30px 30px 0 0; "   
-                                        onerror="this.onerror=null;this.src='/assets/images/404q.png';"
-                                        alt="{{ $product->name }}"
-                                    >
-                                   
-                                </div>
-
-                                <div class="text-start px-3 d-flex flex-column justify-content-between" >
-                                    <div>
-                                        <h5  class="product-card__name text-truncate mt-2 mb-3 px-0 text-decoration-none text-primary d-block mb-1">
-                                            {{ $product->name }}
-                                        </h5>
+                        <a href="{{ route('shop.show', $product->slug) }}" class="text-decoration-none ">
+                            <div style="border-radius: 30px; background-color: rgba(255, 255, 255, 0.7);" class="product-card border border-3 border-primary h-100 d-flex flex-column">
+                                
+                                    <div class="product-card__image-wrapper  mb-3">
                                         
-                                        @if($product->description)
-                                        <div class="product-card__description mb-2 fs-7 ">
-                                            {!! strip_tags($product->description) !!}
-                                        </div>
-                                        @endif
+                                        <img src="{{ asset('storage/' . $product->image) }}" 
+                                            class="product-card__image w-100 object-fit-cover   border-primary border-bottom"
+                                            style="height: 200px;border-radius: 30px 30px 0 0; "   
+                                            onerror="this.onerror=null;this.src='/assets/images/404q.png';"
+                                            alt="{{ $product->name }}"
+                                        >
+                                       
                                     </div>
+                                
 
-                                    <div>
-                                        @php
-                                            $price = number_format($product->price, 2);
-                                            $parts = explode('.', $price);
-                                            $whole = $parts[0];
-                                            $decimal = $parts[1];
-                                        @endphp
-                                        
-                                        <div class="product-card__price text-dark">
-                                            <span class="product-card__currency ">RM</span><span class="product-card__price-main">{{ $whole }}</span><span class="product-card__price-decimal">.{{ $decimal }}</span>
-                                        </div>
-                                        
-                                        <div class="mb-0">
-                                            <small class="text-muted me-2">Category: </small>
-                                            <div class="d-inline-flex flex-wrap justify-content-center align-items-center">
-                                                @foreach($product->categories->take(2) as $category)
-                                                    <a href="{{ '/shop?category=' .  $category->slug }}" class="badge mb-2 bg-primary me-1 text-white text-truncate d-inline-block text-decoration-none" style="width: 12ch; ">
-                                                        {{ $category->name }}
-                                                    </a>
-                                                    
-                                                    
-                                                @endforeach
-                                                 @if($product->isPhysical)
-                                                <span class="badge bg-primary mb-2" style="width: 8ch; ">
-                                                   
-                                                    Physical
-                                                </span>
-                                            @else
-                                                <span class="badge bg-success mb-2" style="width: 8ch; " >
-                                                    
-                                                    Digital
-                                                </span>
-                                            </div>
-                                            @if (isset ($product->categories[2])) 
-                                                ...
-                                            @endif
+                                    <div class="text-start px-3 d-flex flex-column justify-content-between" >
+                                        <div>
+                                                <h5  class="product-card__name text-truncate mt-2 mb-3 px-0 text-primary d-block mb-1">
+                                                    {{ $product->name }}
+                                                </h5>
                                             
-                                        </div>
-                                        
-                                        <div class="mt-2">
-                                           
+                                            @if($product->description)
+                                            <div class="product-card__description mb-2 fs-7 ">
+                                                {!! strip_tags($product->description) !!}
+                                            </div>
                                             @endif
                                         </div>
+
+                                        <div>
+                                            @php
+                                                $price = number_format($product->price, 2);
+                                                $parts = explode('.', $price);
+                                                $whole = $parts[0];
+                                                $decimal = $parts[1];
+                                            @endphp
+                                            
+                                            <div class="product-card__price text-dark">
+                                                <span class="product-card__currency ">RM</span><span class="product-card__price-main">{{ $whole }}</span><span class="product-card__price-decimal">.{{ $decimal }}</span>
+                                            </div>
+                                            
+                                            <div class="mb-0">
+                                                <small class="text-muted me-2">Category: </small>
+                                                <div class="d-inline-flex flex-wrap justify-content-center align-items-center">
+                                                    @foreach($product->categories->take(2) as $category)
+                                                        <a href="{{ '/shop?category=' .  $category->slug }}" class="badge mb-2 bg-primary me-1 text-white text-truncate d-inline-block text-decoration-none" style="width: 12ch; ">
+                                                            {{ $category->name }}
+                                                        </a>
+                                                        
+                                                        
+                                                    @endforeach
+                                                    @if($product->isPhysical)
+                                                    <span class="badge bg-primary mb-2" style="width: 8ch; ">
+                                                    
+                                                        Physical
+                                                    </span>
+                                                @else
+                                                    <span class="badge bg-success mb-2" style="width: 8ch; " >
+                                                        
+                                                        Digital
+                                                    </span>
+                                                @endif
+                                                </div>
+                                                @if (isset ($product->categories[2])) 
+                                                    ...
+                                                @endif
+                                                
+                                            </div>
+
+                                            <!-- View Product Button -->
+                                            <div class="d-flex justify-content-center align-items-center mb-3">
+                                                <a href="{{ route('shop.show', $product->slug) }}" class=" border-bottom border-2 text-muted border-secondary small  px-2 mt-2 ">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi border-secondary bi-eye me-1" viewBox="0 0 16 16">
+                                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                                                    </svg>
+                                                    View Product
+                                                </a>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
                             </div> 
-                             </a>
+                                </a>
+
                         </div>
 
 
