6bd87ff5869fd9a9b0406caf1758b17c2517909a
Ridwan Bin Monjur <mjrrdn@gmail.com>
2025-08-04 20:17:56 +0600

    fix view

--- resources/views/includes/ManageEvent/BracketUpdateList.blade.php
+++ resources/views/includes/ManageEvent/BracketUpdateList.blade.php
@@ -104,46 +104,16 @@
             <p class="mmt-2"> Bracket is not ready till you choose a tier </p>
         @endif
     @elseif ($event?->type?->eventType == "League")
-        <div id="bracket-list" class="custom-scrollbar tab-bracketlist">
+        <div id="bracket-list" class="custom-scrollbar ">
 
-            @if (isset($bracketList['league']['standings']))
-                <h5 class="mb-2 text-start"><u>League Standings</u></h5>
-                <div class="mb-2 row px-0 mx-0">
-                    <div class="col-12">
-                        <div class="table-responsive">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Position</th>
-                                        <th>Team</th>
-                                        <th>Played</th>
-                                        <th>Won</th>
-                                        <th>Draw</th>
-                                        <th>Lost</th>
-                                        <th>Points</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($bracketList['league']['standings'] as $position => $team)
-                                        <tr>
-                                            <td>{{ $position + 1 }}</td>
-                                            <td>{{ $team['name'] }}</td>
-                                            <td>{{ $team['played'] }}</td>
-                                            <td>{{ $team['won'] }}</td>
-                                            <td>{{ $team['draw'] ?? 0 }}</td>
-                                            <td>{{ $team['lost'] }}</td>
-                                            <td>{{ $team['points'] }}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+            {{-- Pagination Component --}}
+            @if(isset($pagination))
+                <x-pagination.league-pagination :pagination="$pagination" />
             @endif
 
+
             @if (isset($bracketList) && !empty($bracketList))
-                <h5 class="mb-2 text-start"><u>Rounds & Results</u></h5>
+                <h5 class="mt-3 mb-2 text-start"><u>Rounds & Results</u></h5>
                 <div class="mb-2 mx-auto px-0 mx-0 ">
 
                     @foreach ($bracketList['table'] as $roundKey => $roundData)
@@ -151,7 +121,7 @@
                             <h6>Round {{ $roundKey }}</h6>
                             <div class="row">
                                 @foreach ($roundData as $matchKey => $match)
-                                    <div class="my-3 col-12 col-lg-6">
+                                    <div class="my-3 col-12 col-md-6 col-xxl-4">
                                         <div>
                                             <div class=" d-none-until-hover2-parent">
                                                 <div class="table-report middle-item {{ $match['team1_position'] }} {{ $match['team2_position'] }} popover-parent "
