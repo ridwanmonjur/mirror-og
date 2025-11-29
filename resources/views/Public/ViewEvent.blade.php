ad0445d549ed3456601f7d6c99df730e6cd721cf
ridwanmonjur <mjrrdn@gmail.com>
2025-11-29 15:28:51 +0600

    fix

--- resources/views/Public/ViewEvent.blade.php
+++ resources/views/Public/ViewEvent.blade.php
@@ -178,13 +178,18 @@
             <div> </div>
             <div>
                 <div class="py-3">
-            <header>
+            <header class="d-flex justify-content-between align-items-center">
                     <h5 class=" py-0 my-0">
                      <u>
                         View your events
                         </u>
                     </h5>
-                </u>
+                    <a href="javascript:history.back()" class="text-decoration-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left me-1" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+                        </svg>
+                        Go back
+                    </a>
             </header>
         </div>
                 <div>
